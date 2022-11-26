b9b5d20c8e14f8b4d73592eacefea19246d09dd0
alejanndrasan <asantos@correo.unimet.edu.ve>
2022-11-26 10:07:42 -0800

    Clase pintura creada

--- pintura.py
+++ pintura.py
@@ -1,1 +1,16 @@
-print("pintura")
+class Pintura:
+  def __init__(self, cota, nombre, precio, status, eliminado):
+    self.cota = cota
+    self.nombre = nombre
+    self.precio = precio
+    self.status = status
+    self.eliminado = eliminado
+
+    def mostrar_pintura(self):
+        if self.eliminado == True:
+             print('''\nCota:{self.cota}
+            \nNombre de la pintura: {self.nombre}
+            \nPrecio: {self.precio}
+            \nStatus: {self.status}}''')
+        else:
+            print('\nLa pinrtura {self.nombre} ha sido eliminada.')
